09c7b3a1d6bcef94efc58247a82d5d66ab7e9ed3
Sarah Barron <sarahflavin@yahoo.com>
2018-05-18 14:17:31 +0000

    added heroku to allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [
+    os.environ.get('C9_HOSTNAME'),
+    'sarah-ecommerce.herokuapp.com'
+    ]
 
 
 # Application definition
